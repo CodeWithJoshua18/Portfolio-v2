d5923c9c73f13e48b3f8e83e9fd5e0d593bcab36
CodeWithJoshua18 <generaljosh5311@gmail.com>
2025-09-26 19:38:35 +0300

    added navbar and hero section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import logo from '../assets/1.png';
+import { FaLinkedin} from 'react-icons/fa';
+import { FaGithub} from 'react-icons/fa';
+import { FaWhatsapp} from 'react-icons/fa';
+
+function Navbar() {
+
+    return(
+        <nav className="bmb-20 flex items-center justify-between py-6">
+            <div className="flex flex-shrink-0 items-center">
+                <img className='mx-2 w-10' src={logo} alt="logo" />
+            </div>
+            <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+                <FaLinkedin />
+                <FaGithub />
+                <FaWhatsapp />
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
