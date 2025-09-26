d3cc74eff44c01ceede2bf00a59a3de74677a6c6
CodeWithJoshua18 <generaljosh5311@gmail.com>
2025-09-26 20:36:20 +0300

    add About component with layout and styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,29 @@
+import { ABOUT_TEXT } from '../constants'
+import aboutImg from '/home/caleb/Documents/React-Projects/React-Portfolio/portfolio/src/assets/3.jpg'
 function About() {
 
     return(
-        <div>About</div>
+        <div className="border-b border-neutral-900 pb-4">
+            <h1 className="my-20 text-center text-4xl">
+                About
+                <span className="text-neutral-500"> Me</span>
+            </h1>
+            <div className="flex flex-wrap">
+                <div className="w-full lg:w-1/2 lg:p-8">
+                    <div className="flex items-center justify-center">
+                        <img className='rounded-2xl shadow-lg shadow-cyan-300/20' 
+                            src={aboutImg} 
+                            alt="Professional portrait of the portfolio owner smiling in a well-lit indoor setting with a neutral background, conveying a friendly and approachable mood" 
+                        />
+                    </div>
+                </div>
+                <div className="w-full lg:w-1/2">
+                        <div className="flex justify-center lg:justify-start">
+                            <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
+                        </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
